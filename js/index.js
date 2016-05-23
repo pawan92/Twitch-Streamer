65c94f648ce4c8ddab40978a2b20ebcc4c80fe28
Pawan <pwanster11@gmail.com>
2016-05-22 22:31:08 -0400

    started js functionality

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+ var channels= ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"]
+  
+  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?'
+  
+})
+
+function callback(data){
+  console.log(data);
+  $('result').
+  
+}
