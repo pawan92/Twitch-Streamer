8a3183210097b5bd3568f322216ad50271522ff3
Pawan <pwanster11@gmail.com>
2016-05-24 20:18:58 -0400

    added offline/online implementation

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
- var channels= ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"]
-  
-  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?'
-  
-})
+ var channels= ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
 
-function callback(data){
-  console.log(data);
-  $('result').
-  
-}
+  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+   console.log(data);
+
+    if (data.stream===null)
+      {
+        console.log("offline");
+      }
+
+    else if (data.stream!==null)
+      {
+        console.log("online");
+      }
+  });
+
+});
