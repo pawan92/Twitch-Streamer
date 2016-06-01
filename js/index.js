faee082a9466987676e882503e8d226b819ff57c
Pawan <pwanster11@gmail.com>
2016-06-01 00:24:09 -0400

    modified JS file

--- js/index.js
+++ js/index.js
@@ -1,18 +1,30 @@
 $(document).ready(function () {
  var channels= ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
 
-  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+function makerequest(x)
+  {
+    $.getJSON('https://api.twitch.tv/kraken/streams/'+channels[x]+'?callback=?', function(data) {
    console.log(data);
+  }
 
+  for (var i=0;i<channels.length;i++)
+  {
+      var twitch=makerequest(i);
+
+      //$('#stream').append(channels[i]);
     if (data.stream===null)
       {
-        console.log("offline");
+
+        $('#result').append('offline');
       }
 
     else if (data.stream!==null)
       {
-        console.log("online");
+
+        $('#result').append('online');
       }
   });
+  }
+
 
 });
