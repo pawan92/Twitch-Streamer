c7bc9f7396eed9c2b9cd4c69e278d5967ee4c4eb
Pawan <pwanster11@gmail.com>
2016-06-05 23:41:54 -0400

    added offline/online capability

--- js/index.js
+++ js/index.js
@@ -1,20 +1,32 @@
-$(document).ready(function () {
- var channels= ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","monstercat"];
+$(document).ready(function() {
+  var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "monstercat", "comster404"];
 
-for (var i=0; i < channels.length; i++) {
-      //var twitch = makerequest(i);
+  for (var i = 0; i < channels.length; i++) {
+    //var twitch = makerequest(i);
     (function(i) {
-        $.getJSON('https://api.twitch.tv/kraken/streams/'+ channels[i] + '?callback=?',   function(data) {
-      console.log(data);
-     //$('#result').append(JSON.stringify(data.stream));
-    if (data.stream === null) {
+      $.getJSON('https://api.twitch.tv/kraken/streams/' + channels[i] + '?callback=?', function(data) {
+        console.log(data);
+        if ( data.status===422) {
+          $('#result').append(
+            '<li>' + channels[i] +' unavailable' + '</li>');
+        }
 
-        $('#result').append('<li>' + channels[i] + ' offline' + '</li>')
-      } else if (data.stream!==null) {
-
-        $('#result').append('<li>' + channels[i] + ' online' + '</li>');
-      }
-  });
+        else if ( data.stream) {
+          $('#result').append(
+            '<a href="http://www.twitch.tv/' + channels[i] + ' target="_blank">' +
+            '<li class="online">' + channels[i] + ' online' + '</li>' +
+            '</a>'
+          );
+        } else {
+          $('#result').append(
+            '<a href="http://www.twitch.tv/' + channels[i] + ' target="_blank">' +
+            '<li>' + channels[i] + ' offline' + '</li>' +
+            '</a>'
+          );
+        }
+      });
     }(i));
-}
+  }
 });
+
+//https://api.twitch.tv/kraken/channels/
